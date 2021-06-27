4ca0f8e8f35dffa49f67d465044a31a3bd6ff408
xkdr1234 <15055905110@qq.com>
2021-06-27 12:28:47 +0800

    changes models(User)

--- users/models.py
+++ users/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 
 class User(models.Model):
     username = models.CharField(max_length=20)
-    password = models.CharField(max_length=64)
+    password = models.CharField(max_length=256)
+
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password, None, 'pbkdf2_sha256')
+        super(User, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
