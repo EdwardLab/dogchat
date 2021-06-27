3766f9c66a23aa512f971bae03dda78d37ed3a80
xkdr1234 <15055905110@qq.com>
2021-06-27 14:22:08 +0800

    Add user registration and login

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.hashers import check_password
+
+from .models import User
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if not username or not password:
+            return render(request, 'users/register.html', {
+                'err_msg':'No username or password entered',
+            })
+        if User.objects.filter(username=username).exists():
+            return render(request, 'users/register.html', {
+                'err_msg':'User already exists',
+            })
+        user = User(username=username, password=password)
+        user.save()
+        return HttpResponse('ok')
+    else:
+        return render(request, 'users/register.html', {})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if not username or not password:
+            return render(request, 'users/login.html', {
+                'err_msg':'No username or password entered',
+            })
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return render(request, 'users/login.html', {
+                'err_msg':'User does not exist',
+            })
+        if check_password(password, user.password):
+            return HttpResponse('ok')
+        return render(request, 'users/login.html', {
+            'err_msg':'wrong user name or password',
+        })
+    else:
+        return render(request, 'users/login.html', {})
