bf3ceacedf428f3750a94d1d91cfedec0f67944c
xkdr1234 <15055905110@qq.com>
2021-06-27 22:55:51 +0800

    fix setting

--- dogchat/settings.py
+++ dogchat/settings.py
@@ -125,7 +125,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static_root/'
-STATICFILES_DIR = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
