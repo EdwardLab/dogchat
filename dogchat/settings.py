f645065aa167d270dccb1cda2c9e4a6cc55f9772
xkdr1234 <15055905110@qq.com>
2021-07-04 20:02:51 +0800

    add homepage(author: xingyujie)

--- dogchat/settings.py
+++ dogchat/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'chat.apps.ChatConfig',
+    'homepage.apps.HomepageConfig',
 ]
 
 MIDDLEWARE = [
--- dogchat/urls.py
+++ dogchat/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('chat/', include('chat.urls')),
+    path('', include('homepage.urls')),
 ]
